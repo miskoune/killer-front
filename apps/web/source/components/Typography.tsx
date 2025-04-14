da38adcb1dcdd5992c2335bfa1d125d2523c5cf1
miskoune <miskoune@proton.me>
2025-04-15 00:38:52 +0200

    refactor: enhance Typography components to accept custom class names and remove unused navigation hook

--- apps/web/source/components/Typography.tsx
+++ apps/web/source/components/Typography.tsx
@@ -1,38 +1,56 @@
-interface Props {
+import { classNames } from '@/helpers/utils';
+
+interface HeadingProps {
   children: JSX.Element | string;
+  className?: string;
 }
 
-function H1({ children }: Props) {
+function H1({ children, className }: HeadingProps) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl whitespace-pre-line">
+    <h1
+      className={classNames(
+        'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl whitespace-pre-line',
+        className,
+      )}
+    >
       {children}
     </h1>
   );
 }
 
-function H2({ children }: Props) {
+function H2({ children, className }: HeadingProps) {
   return (
-    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2
+      className={classNames(
+        'scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+        className,
+      )}
+    >
       {children}
     </h2>
   );
 }
 
-interface H3Props extends React.ComponentPropsWithoutRef<'h3'> {
-  children: string | JSX.Element | JSX.Element[];
-}
-
-function H3({ children }: H3Props) {
+function H3({ children, className }: HeadingProps) {
   return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+    <h3
+      className={classNames(
+        'scroll-m-20 text-2xl font-semibold tracking-tight',
+        className,
+      )}
+    >
       {children}
     </h3>
   );
 }
 
-function Blockquote({ children }: Props) {
+interface BlockquoteProps extends HeadingProps {}
+
+function Blockquote({ children, className }: BlockquoteProps) {
   return (
-    <blockquote className="italic border-zinc-400 text-slate-400">
+    <blockquote
+      className={classNames('italic border-zinc-400 text-slate-400', className)}
+    >
       {children}
     </blockquote>
   );
