f00c9be8ee30f4781aa852c1cdb953ed5a51bb65
miskoune <miskoune@proton.me>
2025-04-15 23:07:17 +0200

    chore: update sonner dependency to version 2.0.3 and adjust Toaster component styles for improved UI consistency

--- apps/web/source/components/Toaster.tsx
+++ apps/web/source/components/Toaster.tsx
@@ -7,19 +7,17 @@ interface ToasterProps extends ComponentProps<typeof Sonner> {}
 export function Toaster({ ...props }: ToasterProps) {
   return (
     <Sonner
-      className="bg-"
+      closeButton
       icons={{
         success: <CircleCheck className="stroke-white w-6 h-6" />,
         error: <CircleX className="stroke-white w-6 h-6" />,
       }}
       toastOptions={{
         classNames: {
-          toast: 'transition duration-500 hover:scale-105',
-          success: 'bg-emerald-500',
-          error: 'bg-red-500',
-          title: 'ml-2 text-white text-m font-normal',
-          description: 'ml-2 text-white/90 text-sm',
-          cancelButton: 'cursor-pointer',
+          closeButton: '!bg-white/80 hover:!bg-white !transition !duration-500',
+          success: '!bg-emerald-500 !border-none',
+          error: '!bg-red-500 !border-none',
+          title: 'ml-2 !text-white/80 !text-sm',
         },
       }}
       {...props}
