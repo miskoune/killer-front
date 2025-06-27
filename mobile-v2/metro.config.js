5129871f61e5602640b5286404d4dc268abf5191
miskoune <miskoune@proton.me>
2025-06-27 17:50:29 +0200

    refactor: update app icon and splash image

--- mobile-v2/metro.config.js
+++ mobile-v2/metro.config.js
@@ -1,20 +1,16 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
-
-  config.transformer = {
-    ...transformer,
+module.exports = {
+  ...defaultConfig,
+  transformer: {
+    ...defaultConfig.transformer,
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  };
-
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
-  };
-
-  return config;
-})();
+  },
+  resolver: {
+    ...defaultConfig.resolver,
+    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+};
