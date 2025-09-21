b12538fe7e04c299208895f2748c87eed6a2892a
miskoune <miskoune@proton.me>
2025-09-21 12:17:02 +0200

    feat: add Pseudo onboarding screen with input for player name and navigation to avatar selection

---
+++ mobile-v2/app/onboarding/index.tsx
@@ -0,0 +1,92 @@
+import { Image } from 'expo-image';
+import { useRouter } from 'expo-router';
+import { createRef } from 'react';
+import {
+  View,
+  type TextInput,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { Button } from '@/components/Button';
+import { Header } from '@/components/Header';
+import { Input } from '@/components/Input';
+import { COLORS } from '@/constants/theme';
+import { selectPlayer, selectUpdatePlayer } from '@/selectors/player';
+import { usePlayerStore } from '@/store/player';
+
+export default function Pseudo() {
+  const player = usePlayerStore(selectPlayer);
+  const updatePlayer = usePlayerStore(selectUpdatePlayer);
+  const inputRef = createRef<TextInput>();
+  const router = useRouter();
+  const insets = useSafeAreaInsets();
+
+  return (
+    <KeyboardAvoidingView
+      style={[styles.content]}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : 0}
+    >
+      <ScrollView contentContainerStyle={[styles.scrollViewContent]}>
+        <Header title="Choisir un pseudo" />
+        <TouchableWithoutFeedback onPress={() => inputRef.current?.blur()}>
+          <View style={[styles.view]}>
+            <Image
+              source={require('@/assets/images/pseudo.png')}
+              style={styles.lottie}
+            />
+            <Input
+              label="Pseudo"
+              value={player?.name ?? ''}
+              setValue={(name) => updatePlayer({ name })}
+            />
+          </View>
+        </TouchableWithoutFeedback>
+
+        <View style={[styles.buttonContainer]}>
+          <Button
+            disabled={!player?.name}
+            color="primary"
+            onPress={() => router.push('/onboarding/choose-avatar')}
+            text="Suivant"
+            customStyle={{ marginBottom: insets.bottom }}
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBackgroundColor,
+  },
+  view: {
+    display: 'flex',
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+  buttonContainer: {
+    padding: 20,
+    paddingBottom: 20,
+  },
+  lottie: {
+    width: 220,
+    height: 220,
+    marginTop: -20,
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+});
